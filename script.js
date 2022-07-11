4788ef7ac61852e2e15751d6ff484ecde367c188
Julieana Finch <julieanamichelle@hotmail.com>
2022-07-11 15:03:34 -0500

    workingcode

--- script.js
+++ script.js
@@ -1,17 +1,68 @@
 // Assignment code here
 
-
 // Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+var password = document.getElementById("password");
 
-  passwordText.value = password;
+function genPassword() {
+  //Get password length
 
+  var passwordLength = parseInt(
+    prompt(
+      "Choose a password length of at least 8 characters and no more than 128 characters"
+    ),
+    10
+  );
+  if (isNaN(passwordLength)) {
+    alert("Please eneter a number");
+
+    //User input character selections
+  } else {
+    var includeSpecialCharacters = confirm(
+      "Click ok if you would like $pecial characters. Click Cancel if you would not."
+    );
+    var includeLowercase = confirm(
+      "Click ok if you would like to include lowercase letters. Click cancel if you would not."
+    );
+    var includeUppercase = confirm(
+      "Click ok if you would like to include UPPERCASE letters. Click cancel if you would not."
+    );
+    var includeNumbers = confirm(
+      "Click ok if you would like to unclude numbers. Click cancel if you would not."
+    );
+    if (
+      !includeLowercase &&
+      !includeUppercase &&
+      !includeSpecialCharacters &&
+      !includeNumbers
+    ) {
+      alert("Please select at least one");
+    }
+
+    //Select characters
+
+    var chars = "";
+    if (includeLowercase) {
+      chars += "abcdefghijklmnopqrstuvwxyz";
+    }
+    if (includeUppercase) {
+      chars += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    }
+    if (includeNumbers) {
+      chars += "0123456789";
+    }
+    if (includeSpecialCharacters) {
+      chars += "!@#$%^&*?";
+    }
+
+    //Generate Password
+
+    var password = "";
+
+    for (var i = 0; i < passwordLength; i++) {
+      var randomNumber = Math.floor(Math.random() * chars.length);
+      password += chars.charAt(randomNumber, randomNumber);
+    }
+    document.getElementById("password").value = password;
+  }
 }
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
